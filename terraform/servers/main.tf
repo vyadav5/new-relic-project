27f01191b70a99190fe0dcd5978b4e57e1e4b7c8
Vidhi Yadav <79339896+vyadav5@users.noreply.github.com>
2023-12-22 03:38:15 +0530

    Update main.tf

--- terraform/servers/main.tf
+++ terraform/servers/main.tf
@@ -68,5 +68,5 @@ resource "aws_key_pair" "key_pair" {
 # download key in system
 resource "local_file" "private_key" {
   content = tls_private_key.rsa_4096.private_key_pem
-  filename = var.key_name
+  filename = var.key_path
 }
