f1b0239786b2615e3743bfcb0bf1a6280119b1f9
Vidhi Yadav <79339896+vyadav5@users.noreply.github.com>
2023-12-23 01:53:05 +0530

    Update script_newrelic.sh

--- Bash_script/script_newrelic.sh
+++ Bash_script/script_newrelic.sh
@@ -13,6 +13,7 @@ INSTALL_FILE="$HOME/newrelic-infra/installer.sh"
 # Run the New Relic installation command
 V="$V" ARCH="$ARCH"; echo "https://download.newrelic.com/infrastructure_agent/binaries/linux/${ARCH}/newrelic-infra_linux_${V}_${ARCH}.tar.gz" | { read    url; wget "${url}"{,.sum}; shasum -a 256 --check ${url##*/}.sum; }
 
+#unzip tar
 echo "Downloading New Relic tar file..."
 tar -xzvf "$DOWNLOAD_PATH"
 
